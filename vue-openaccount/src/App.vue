1404d83f626f23d2b6e1b6d7ecc91b456b282420
LiuRH <1393991675@qq.com>
2019-06-11 14:56:13 +0800

    安装依赖和主题

--- vue-openaccount/src/App.vue
+++ vue-openaccount/src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <transition name='fade' mode='out-in'>
+      <router-view></router-view>
+    </transition>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -7,11 +10,20 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  components: {}
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+  font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
+  font-size: 14px;
+  -webkit-font-smoothing: antialiased;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -19,5 +31,34 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.el-submenu [class^=fa] {
+  vertical-align: baseline;
+  margin-right: 10px;
+}
+ 
+.el-menu-item [class^=fa] {
+  vertical-align: baseline;
+  margin-right: 10px;
+}
+ 
+.toolbar {
+  background: #f2f2f2;
+  padding: 10px;
+  margin: 10px 0;
+  .el-form-item {
+      margin-bottom: 10px;
+  }
+}
+ 
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+ 
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
 }
 </style>
