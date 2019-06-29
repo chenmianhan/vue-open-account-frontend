4b137157ec4f0c02ba69d3f78bd4be2c860dc18f
EIHU <hky1998@outlook.com>
2019-06-29 11:25:38 +0800

    暂时删除登录拦截功能

--- vue-openaccount/src/main.js
+++ vue-openaccount/src/main.js
@@ -30,58 +30,58 @@ new Vue({
   template: '<App/>'
 });
 
-router.beforeEach((to, from, next) => {
-  let getFlag = localStorage.getItem('Flag');//获取用户登录成功后储存的登陆标志
-  let getRole = localStorage.getItem('Role');//获取用户的身份标记
+// router.beforeEach((to, from, next) => {
+//   let getFlag = localStorage.getItem('Flag');//获取用户登录成功后储存的登陆标志
+//   let getRole = localStorage.getItem('Role');//获取用户的身份标记
 
-  //如果登录标志存在且为isLogin,则用户已经登陆
-  if (getFlag === 'isLogin'){
-    //设置vuex登录状态为已登录
-    store.state.isLogin = true;
-    next();
-    //如果已登录，还要选择回到登录界面，则定向回到用户首页
-    if(!to.meta.isLogin){
-      ElementUI.Message({//消息提示退出登录
-        showClose: true,
-        message: "请先退出登录！",
-        duration: 5000,
-        type: 'error'
-      });
+//   //如果登录标志存在且为isLogin,则用户已经登陆
+//   if (getFlag === 'isLogin'){
+//     //设置vuex登录状态为已登录
+//     store.state.isLogin = true;
+//     next();
+//     //如果已登录，还要选择回到登录界面，则定向回到用户首页
+//     if(!to.meta.isLogin){
+//       ElementUI.Message({//消息提示退出登录
+//         showClose: true,
+//         message: "请先退出登录！",
+//         duration: 5000,
+//         type: 'error'
+//       });
 
-      switch(getRole){//根据身份标识跳转
-        case '0':
-          next({ path: '/user/home'});
-          break;
-        case '1':
-          next({ path: ''});//审核员界面待添加
-          break;
-        case '2':
-          next({ path: '/admin/home'});
-          break;
-        case '3':
-          next({ path: ''})//超管界面待添加
-      }
-    }
-  }else{//如果未登录
-      //想进入需要登录的界面，则定向回到登录界面
-      if(to.meta.isLogin){
-        next({ path: '/login' });
-        ElementUI.Message({//消息提示请先登录
-          showClose: true,
-          message: "请先登录！",
-          duration: 5000,
-          type: 'error'
-        });
-      }else{
-        //用户进入无需登录的界面，跳转继续
-        next()
-      }
-    }
-});
+//       switch(getRole){//根据身份标识跳转
+//         case '0':
+//           next({ path: '/user/home'});
+//           break;
+//         case '1':
+//           next({ path: '/reviewer/home'});//审核员界面待添加
+//           break;
+//         case '2':
+//           next({ path: '/admin/home'});
+//           break;
+//         case '3':
+//           next({ path: '/superadmin/home'})//超管界面待添加
+//       }
+//     }
+//   }else{//如果未登录
+//       //想进入需要登录的界面，则定向回到登录界面
+//       if(to.meta.isLogin){
+//         next({ path: '/login' });
+//         ElementUI.Message({//消息提示请先登录
+//           showClose: true,
+//           message: "请先登录！",
+//           duration: 5000,
+//           type: 'error'
+//         });
+//       }else{
+//         //用户进入无需登录的界面，跳转继续
+//         next()
+//       }
+//     }
+// });
 
-router.afterEach(route => {
-  window.scroll(0,0);//滚动条到最顶部
-})
+// router.afterEach(route => {
+//   window.scroll(0,0);//滚动条到最顶部
+// })
 
 
 
--- vue-openaccount/src/router/index.js
+++ vue-openaccount/src/router/index.js
@@ -14,62 +14,62 @@ export default new Router({
       path: '/login',
       component: resolve => require(['../pages/Login.vue'], resolve),
       meta:{
-        isLogin: false//不需登录即可进入
+        //isLogin: false//不需登录即可进入
       },
       name: '登录'
     },{
       path: '/user',
       component: resolve => require(['../components/beforecheckMain.vue'], resolve),
       meta:{
-        isLogin: true
+        //isLogin: true
       },
       children: [{
         path: '/user/loading',
         component: resolve => require(['../pages/user/beforecheck/loading.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '待审核'
       }, {
         path: '/user/notPass',
         component: resolve => require(['../pages/user/notPass.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '未通过'
       }, {
         path: '/user/choose',
         component: resolve => require(['../pages/user/beforecheck/choose.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '选择营业网点'
       }, {
         path: '/user/evaluation',
         component: resolve => require(['../pages/user/beforecheck/evaluation.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '风险测评'
       },{
         path: '/user/inputInfo',
         component: resolve => require(['../pages/user/beforecheck/inputInfo.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '填写资料'
       }, {
       path: '/login/warning',
       component: resolve => require(['../pages/user/beforecheck/warning.vue'], resolve),
       meta:{
-        isLogin: true//需要登录
+        //isLogin: true//需要登录
       },
       name: '风险提示'
     },{
         path: '/user/chooseBank',
         component: resolve => require(['../pages/user/beforecheck/chooseBank.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '选择银行'
       }]
@@ -77,27 +77,27 @@ export default new Router({
       path: '/user/home',
       component: resolve => require(['../components/Main.vue'], resolve),
       meta:{
-        isLogin: true
+        //isLogin: true
       },
       children: [{
         path: '/user/home',
         component: resolve => require(['../pages/user/login/myAccounts.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '我的账户'
       }, {
         path: '/user/info',
         component: resolve => require(['../pages/user/login/personalInfo.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '个人信息'
       }, {
         path: '/user/lineDetail',
         component: resolve => require(['../pages/user/login/lineDetail.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '流水明细'
       }]
@@ -107,20 +107,20 @@ export default new Router({
       path: '/reviewer/home',
       component: resolve => require(['../components/reviewerMain.vue'], resolve),
       meta:{
-        isLogin: true
+        //isLogin: true
       },
       children:[{
         path:'/reviewer/home',
         component: resolve => require(['../pages/reviewer/statisticData.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '统计数据'
       },{
         path:'/reviewer/reviewUser',
         component: resolve => require(['../pages/reviewer/reviewUser.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '开始审核'
       }]
@@ -130,20 +130,20 @@ export default new Router({
       path: '/admin',
       component: resolve => require(['../components/adminMain.vue'], resolve),
       meta:{
-        isLogin: true
+        //isLogin: true
       },
       children:[{
         path:'/admin/home',
         component: resolve => require(['../pages/admin/userManagement.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '用户管理'
       },{
         path:'/admin/manageReviewer',
         component: resolve => require(['../pages/admin/reviewerManagement.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '审核员管理'
       }]
@@ -153,27 +153,27 @@ export default new Router({
       path: '/superadmin',
       component: resolve => require(['../components/superAdminMain.vue'], resolve),
       meta:{
-        /*isLogin: true*/
+        /*//isLogin: true*/
       },
       children:[{
         path:'/superadmin/home',
         component: resolve => require(['../pages/superAdmin/userManagement.vue'], resolve),
         meta:{
-          /*isLogin: true*///需要登录
+          /*//isLogin: true*///需要登录
         },
         name: '用户管理'
       },{
         path:'/superadmin/manageAdmin',
         component: resolve => require(['../pages/superAdmin/adminManagement.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '管理员管理'
       },{
         path:'/superadmin/manageInstitute',
         component: resolve => require(['../pages/superAdmin/instituteManagement.vue'], resolve),
         meta:{
-          isLogin: true//需要登录
+          //isLogin: true//需要登录
         },
         name: '机构管理'
       }]
