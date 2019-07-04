463d334bb2b98ce2f28750fd2dc894a186ec23f3
BlakeAC <38423136+BlakeAC@users.noreply.github.com>
2019-07-04 15:50:48 +0800

    修复bug

--- vue-openaccount/src/components/adminMain.vue
+++ vue-openaccount/src/components/adminMain.vue
@@ -44,6 +44,7 @@
                   {{adminName}} <i class="el-icon-caret-bottom"></i>
                 </span>
               <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item disabled="true">{{netName}}</el-dropdown-item>
                 <el-dropdown-item command="loginout">退出登录</el-dropdown-item>
               </el-dropdown-menu>
             </el-dropdown>
