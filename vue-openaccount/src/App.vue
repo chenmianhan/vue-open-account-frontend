f353de35a5a076b591425811760be9725dd09422
Julie1231 <1393991675@qq.com>
2019-07-05 16:20:51 +0800

    超管

--- vue-openaccount/src/App.vue
+++ vue-openaccount/src/App.vue
@@ -20,6 +20,7 @@ body {
   font-family: Helvetica Neue, Helvetica, PingFang SC, Hiragino Sans GB, Microsoft YaHei, SimSun, sans-serif;
   font-size: 14px;
   -webkit-font-smoothing: antialiased;
+  /* background: #2c3e50; */
 }
 
 #app {
